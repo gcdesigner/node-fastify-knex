c2f69de5647ceb5cf53aad0da5f064308486896b
gcdesigner <gcdesigner.contato@gmail.com>
2024-10-22 19:57:58 -0300

    chore: production config

--- src/database.ts
+++ src/database.ts
@@ -2,18 +2,17 @@ import { knex as setupNex, type Knex } from "knex";
 import { env } from "./env";
 
 export const config: Knex.Config = {
-  client: "sqlite3",
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
     directory: "./db/migrations",
-  },
-  seeds: {
-    extension: "ts",
-    directory: "./db/seeds",
   },
 };
 
